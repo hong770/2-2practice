467e0c6c8b3962a6711a16400494bdb06b4bde9a
ak416016@gmail.com <ak416016@gmail.com>
2021-03-30 16:46:27 +0800

    s

---
+++ 2-2practice/判斷閨年.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="判斷閨年.aspx.cs" Inherits="_2_2practice.判斷閨年" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 2-2practice/判斷閨年.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2_2practice
+{
+    public partial class 判斷閨年 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int N = 300;
+            if (N % 400 == 0)
+            {
+                Response.Write("Y");
+            }
+            else if (N % 4 == 0 & N % 100 == 0)
+            {
+                Response.Write("N");
+            }
+            else if (N % 4 == 0 & N % 100 != 0)
+            {
+                Response.Write("Y");
+            }
+            else
+            {
+                Response.Write("N");
+            }
+        }
+    }
+}
